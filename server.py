c9a05e22c3c24782fa7e4452f6b7008c32dba3b6
Priyanta Islam <priyantaislam@gmail.com>
2022-07-10 17:54:29 +1000

    added server functions

--- server.py
+++ server.py
@@ -10,16 +10,48 @@ server.bind((host,port))
 server.listen()
 
 clients = []
-nicknames = []
+usernames = []
 
 #broadcast
 def broadcast(message):
         for client in clients:
                 client.send(message)
 
+#handle
+def handle(client):
+        while True:
+                try:
+                        message = client.recv(1024)
+                        print(f"{nicknames[clients.index(client)]} says {message}")
+                        broadcast(message)
+
+                except:
+                        index = clients.index(client)
+                        clients.remove(client)
+                        client.close()
+                        uname = usernames[index]
+                        usernames.remove(uname)
+
+
 #receive
 def receive():
         while True:
                 client, address = server.accept()
-                print(f"{str(address)} just joined the chat!")
+                print(f"Connected with {str(address)}!\n")
+        
+        client.send("USRN".encode('utf-8'))
+        uname = client.recv(1024)
+        
+        nicknames.append(uname)
+        clients.append(client)
 
+        print(f"Username of the client is {uname}!\n")
+        broadcast(f"{uname} just joined the chat!\n".encode('utf-8'))
+        client.send("Connected to the server!\n".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server running . . .")
+receive()
