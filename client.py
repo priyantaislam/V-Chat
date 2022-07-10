5287ea02f723ea15ce24bb27728eb27cf749b07c
Priyanta Islam <priyantaislam@gmail.com>
2022-07-10 22:19:44 +1000

    completed client side

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import threading
 import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
+#import faulthandler; faulthandler.enable()
 
 HOST = '127.0.0.1'
-PORT = 9999
+PORT = 9090
 
 class Client:
 
@@ -14,7 +15,7 @@ class Client:
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.sock.connect((host,port))
 
-                msg = tkinter.TK()
+                msg = tkinter.Tk()
                 msg.withdraw()
 
                 self.uname = simpledialog.askstring("Username", "Please choose a username", parent=msg)
@@ -25,11 +26,12 @@ class Client:
                 gui_thread = threading.Thread(target=self.gui_loop)
                 recv_thread = threading.Thread(target=self.receive)
 
-                gui_thread.start()
                 recv_thread.start()
+                gui_thread.start()
+                
 
         def gui_loop(self):
-                self.win = tkinter.TK()
+                self.win = tkinter.Tk()
                 self.win.configure(bg="lightgray")
                 
                 self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
@@ -40,9 +42,52 @@ class Client:
                 self.text_area.pack(padx=20,pady=5)
                 self.text_area.config(state='disabled')
 
-                
+                self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
+                self.msg_label.config(font=("Arial",12))
+                self.msg_label.pack(padx=20,pady=5)
 
+                self.input_area = tkinter.Text(self.win, height=5)
+                self.input_area.pack(padx=20,pady=5)
+
+                self.send_button = tkinter.Button(self.win, text="Send", command = self.write)
+                self.send_button.config(font=("Arial",12))
+                self.send_button.pack(padx=20,pady=5)
+
+                self.gui_done = True
+
+                self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+                self.win.mainloop()
         
+        def write(self):
+                message = f"{self.uname}: {self.input_area.get('1.0','end')}"
+                self.sock.send(message.encode('utf-8'))
+                self.input_area.delete('1.0','end')
+
+        def stop(self):
+                self.running = False
+                self.win.destroy()
+                self.sock.close()
+                exit(0)
 
         def receive(self):
-                pass
+                while self.running:
+                        try:
+                                message = self.sock.recv(1024).decode('utf-8')
+                                if message == "USRN":
+                                        print("reached here")
+                                        self.sock.send(self.uname.encode('utf-8'))
+                                else:
+                                        if self.gui_done:
+                                                self.text_area.config(state='normal')
+                                                self.text_area.insert('end', message)
+                                                self.text_area.yview('end')
+                                                self.text_area.config(state='disabled')
+                        except ConnectionAbortedError:
+                                break
+                        except:
+                                print("Error")
+                                self.sock.close()
+                                break        
+
+client = Client(HOST,PORT)
